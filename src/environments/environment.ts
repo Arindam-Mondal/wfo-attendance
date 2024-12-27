e97295f64f6b8d21fdeb9ed2120588fa936181ee
Arindam-Mondal <ariron123@gmail.com>
2024-12-27 12:12:12 +0530

    using @ngx-env/builder for env variables.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
-declare const process: any;
 
 export const environment = {
   production: false,
-  apiKey: process.env['FB_API_KEY'],
-  authDomain: process.env['FB_AUTH_DOMAIN'],
-  projectId: process.env['FB_PROJECT_ID'],
-  storageBucket: process.env['FB_STORAGE_BUCKET'],
-  messagingSenderId: process.env['FB_MESSAGING_SENDER_ID'],
-  appId: process.env['FB_APP_ID']
+  apiKey: import.meta.env.FB_API_KEY,
+  authDomain: import.meta.env.FB_AUTH_DOMAIN,
+  projectId: import.meta.env.FB_PROJECT_ID,
+  storageBucket: import.meta.env.FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FB_APP_ID
 };
 
